97b393f46573bc840adbe00ea1f171e5987d905c
Emerita Medea <emedea@oa-k1.21-school.ru>
2019-08-20 18:52:09 +0300

    changes

--- includes/errors.h
+++
@@ -1,6 +0,0 @@
-#ifndef ERRORS_H
-# define ERRORS_H
-# define ERROR_DIGITS       1
-# define ERROR_DUPLICATE    2
-# define ERROR_MALLOC       100
-#endif
--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,46 +6,49 @@
 /*   By: emedea <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/15 16:30:44 by emedea            #+#    #+#             */
-/*   Updated: 2019/08/20 18:40:08 by emedea           ###   ########.fr       */
+/*   Updated: 2019/08/20 18:47:43 by emedea           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+# define ERROR_DIGITS		1
+# define ERROR_DUPLICATE	2
+# define ERROR_MALLOC		100
 
-typedef struct		s_list{
-	int		        data;
-	struct s_list	*next;
-}			        t_list;
+typedef struct				s_list{
+	int		        		data;
+	struct s_list			*next;
+}			        		t_list;
 
 /*
  * Operations
  */
-void				swap(t_list *list);
-void				rotate(t_list *list);
+void						swap(t_list *list);
+void						rotate(t_list *list);
 
 /*
  * Validation
  */
-int			        validation_number(char **argv);
-int			        validation_duplicate(char **argv);
+int			        		validation_number(char **argv);
+int			        		validation_duplicate(char **argv);
 
 /*
  * List
  */
-t_list				*new_list(char **argv);
-void				delete(t_list *list);
-void				delete_all(t_list *list);
+t_list						*new_list(char **argv);
+void						delete(t_list *list);
+void						delete_all(t_list *list);
 
 /*
  * Errors
  */
-int              	push_swap_log(int e);
-int                 print(t_list *list);
+int              			push_swap_log(int e);
+int                 		print(t_list *list);
 
 /*
  * Misc
 */
-int                 ft_atoi(char *str);
+int                 		ft_atoi(char *str);
 
 #endif
